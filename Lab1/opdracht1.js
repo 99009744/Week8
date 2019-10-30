9b24f1615e389f1b1f78ddd996642e87db8c9d70
99009744 <99009744@mydavinci.nl>
2019-10-30 10:54:16 +0100

    lab 1&2 af
    lab 1&2 af

---
+++ Lab1/opdracht1.js
@@ -0,0 +1,7 @@
+function halloWorld(number){ 
+    for (let i=1; i<number; i++)
+    document.write(i +". Hallo World!<br>");
+ 
+ }
+  
+ halloWorld(7);
---
+++ Lab1/opdracht2.js
@@ -0,0 +1,47 @@
+function addition(a, b){
+    var c = a+b;
+    document.write (a+"+"+b+"="+c+"<br>");
+}
+addition(12,99);
+addition(44,21);
+addition(71,31);
+
+function subtraction(d, e){
+    var f = d-e;
+    document.write (d+"-"+e+"="+f+"<br>");
+}
+subtraction(42,11);
+subtraction(91,67);
+subtraction(12,3);
+
+function multiplication(g, h){
+    var i = g*h;
+    document.write (g+"x"+h+"="+i+"<br>");
+}
+multiplication(4,11);
+multiplication(9,6);
+multiplication(5,3);
+
+function division(j, k){
+    var l = j*k;
+    document.write (j+":"+k+"="+l+"<br>");
+}
+division(400,10);
+division(90,30);
+division(6,3);
+
+function increment(m){
+    var n = m+1;
+    document.write (m+"+1="+n+"<br>");
+}
+increment(199);
+increment(250);
+increment(50);
+
+function decrement(o){
+    var p = o-1;
+    document.write (o+"-1="+p+"<br>")
+}
+decrement(54);
+decrement(12);
+decrement(76);
